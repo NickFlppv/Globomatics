61f0c9d4bc407673780b33ccf5610823aae11ba0
Николай Филиппов <nikolay.filippov.1999@mail.ru>
2019-05-16 06:55:37 +0300

    Added ConferenceController

---
+++ Globomatics/Controllers/ConferenceController.cs
@@ -0,0 +1,42 @@
+﻿using Globomatics.Services;
+using Microsoft.AspNetCore.Mvc;
+using Shared.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Globomatics.Controllers
+{
+    public class ConferenceController : Controller
+    {
+        private readonly IConferenceService conferenceService;
+
+        public ConferenceController(IConferenceService conferenceService)
+        {
+            this.conferenceService = conferenceService;
+        }
+
+        public async Task<IActionResult> Index()
+        {
+            ViewBag.Title = "All conferences";
+            return View(await conferenceService.GetAll());
+        }
+
+        public IActionResult Add()
+        {
+            ViewBag.Title = "Add conference";
+            return View(new ConferenceModel());
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> Add(ConferenceModel model)
+        {
+            if (ModelState.IsValid)
+            {
+                await conferenceService.Add(model);
+            }
+            return RedirectToAction("Index");
+        }
+    }
+}
