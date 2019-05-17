1f0932ab2bdef21f20366180129bfde6438e061f
Николай Филиппов <nikolay.filippov.1999@mail.ru>
2019-05-17 08:41:28 +0300

    Added StatisticsViewComponent

---
+++ Globomatics/ViewComponents/StatisticsViewComponent.cs
@@ -0,0 +1,24 @@
+﻿using Globomatics.Services;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Globomatics.ViewComponents
+{
+    public class StatisticsViewComponent : ViewComponent
+    {
+        private readonly IConferenceService conferenceService;
+
+        public StatisticsViewComponent(IConferenceService conferenceService)
+        {
+            this.conferenceService = conferenceService;
+        }
+        public async Task<IViewComponentResult> InvokeAsync()
+        {
+            var statistics = await conferenceService.GetStatistics();
+            return View(statistics);
+        }
+    }
+}
--- Shared/Models/StatisticsModel.cs
+++ Shared/Models/StatisticsModel.cs
@@ -1,12 +1,15 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Text;
 
 namespace Shared.Models
 {
     public class StatisticsModel
     {
+        [Display(Name ="Averege amount of attendees on conference")]
         public int AverageConferenceAttendees { get; set; }
+        [Display(Name ="Number of Attendees on conferences")]
         public int NumberOfAttendees { get; set; }
     }
 }
